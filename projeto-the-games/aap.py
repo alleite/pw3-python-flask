976c2b80b83385ea93d7cbabc682fb3db98a783f
analeite <analuiza21.rosario@gmail.com>
2025-02-21 11:46:36 -0300

    lista de games

--- projeto-the-games/aap.py
+++ projeto-the-games/aap.py
@@ -1,20 +1,28 @@
-#Importando o pacote do Flask 
-from flask import Flask
+# Importando o pacote do Flask
+from flask import Flask, render_template
 
-#Carregando o flask na variável app
-app = Flask(__name__)
+# Carregando o flask na variável app
+app = Flask(__name__, template_folder='views')
 
 
-#Criando a rota principal do site
+# Criando a rota principal do site
 @app.route('/')
-#Criando função no python
+# Criando função no python
+# View Fuction - Função de visualização
 def home():
-    return '<h1>Oi =) </h1>'
+    return render_template('index.html')
+
 
 @app.route('/games')
 def games():
-    return'<h1> Seja bem-vindo a página de games</h1>'
+    titulo = 'minecraft'
+    ano = 2009
+    categoria = 'sandbox e sobrevivencia'
+    jogadores = ['queen' , 'bely' , 'pep' , 'luph', 'deca' ]
+    jogosOnline = ['Fall Guys' , 'Mortal Kombat' , 'Street Figther' , 'Tekken', 'Super Mario', 'Tetris', 'HayDay' ]
+    return render_template('games.html',titulo=titulo, ano=ano, categoria=categoria, jogadores=jogadores, jogosOnline=jogosOnline )
+
 
 if __name__ == '__main__':
-    #Rodando o servidor do localhost, porta 5000 
-    app.run(host='localhost', port=5000, debug=True )
+    # Rodando o servidor do localhost, porta 5000
+    app.run(host='localhost', port=5000, debug=True)
